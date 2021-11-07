bd20e09155d3b6cf8a0f7c03b057a4c234cc59e7
MarieArouet <92182959+MarieArouet@users.noreply.github.com>
2021-11-07 22:06:29 +0500

    Practising with dictionaries

--- TestingFile.py
+++ TestingFile.py
@@ -1,6 +1,18 @@
-dic = {'m': 10, 'n': 3, 's': 15}
+hand = open("styles.css")
+counts = dict()
+for line in hand:
+    words = line.split()
+    for word in words:
+        counts[word] = counts.get(word, 0) + 1
+
+print([k,v = counts.item()])
 lis = list()
-for k, v in dic.items():
-    lis.append((v, k))
+for k, v in counts.items():
+    lis.append((v,k))
 lis = sorted(lis, reverse=True)
-print(lis)
+
+for v, k in lis[:10]:
+    print(k, v)
+
+value = [1, 2, 3, 4, 5]
+print(value[2])
